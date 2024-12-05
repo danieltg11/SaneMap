bc95361d474bd2081d85baffeaf819065730a39e
Legobytes <146657080+danieltg11@users.noreply.github.com>
2024-12-04 23:17:27 -0300

    Add files via upload

---
+++ fonte.js
@@ -0,0 +1,22 @@
+function mandawpp(){
+    var numerotelefone = '+553799096880'
+
+    var nome = document.querySelector('.pessoaname').value
+    var cidade = document.querySelector('.cityname').value
+    var endereço = document.querySelector('.endereço').value
+    var sn = document.querySelector('.sn').value
+    var informações = document.querySelector('.infoadicionais').value
+
+     var url = "https://wa.me/" + numerotelefone + "?text="
+    +"*QUEIXA SOBRE O SANEAMENTO BÁSICO*"+ "%0a"
+    +"Nome: " +nome+ "%0a"
+    +"Cidade: " +cidade+ "%0a"
+    +"Endereço do incidente: " +endereço+ "%0a"
+    +"Solicita uma estação de tratamento:" +sn+ "%0a"
+    +"Informções adicionais: " +informações+ "%0a"
+    +"*Muito obrigado por nos reportar problemas no saneamento* "
+
+    window.open(url, '_blank').focus()
+
+
+}
